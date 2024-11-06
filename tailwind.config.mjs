624521139711f685f7f5fa0fb80ff947a3d8444a
Mario <marioodevv@gmail.com>
2024-11-06 16:20:55 +0100

    welcome-page

---
+++ tailwind.config.mjs
@@ -0,0 +1,32 @@
+import { custom } from 'astro:schema';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        mochi: ['Mochiy', 'sans-serif'],
+      },
+    },
+    colors: {
+      'steel-blue': {
+        '50': '#f2f7fd',
+        '100': '#e5edf9',
+        '200': '#c5daf2',
+        '300': '#91bbe8',
+        '400': '#5698da',
+        '500': '#327fcd',
+        '600': '#2160a8',
+        '700': '#1c4d88',
+        '800': '#1b4271',
+        '900': '#1b395f',
+        '950': '#12243f',
+      },
+    },
+  },
+  plugins: [
+    require('tailwindcss-animated')
+  ],
+}
